28dd7271bc2adc71f61236555299a3a9c4b00259
Pushpendra kumar <pushpendra@kmi.world>
2019-06-11 17:32:21 +0530

    Data Insertion and Data listing.

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -145,7 +145,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 498);
             this.Controls.Add(this.txtPinCode);
             this.Controls.Add(this.txtCountry);
             this.Controls.Add(this.txtAddress);
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -14,25 +14,55 @@ namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
     {
-        private string cs = ConfigurationManager.ConnectionStrings["data"].ConnectionString;
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
         public Form1()
         {
             InitializeComponent();
             DataShow();
         }
+
         public void DataShow()
         {
-            SqlConnection con = new SqlConnection(cs);
-            SqlCommand cmd = new SqlCommand();
-
+            using (con = new SqlConnection(cs))
+            {
+                string command = "select * from [dbo].[Addmission]";
+                using (cmd = new SqlCommand(command, con))
+                {
+                    con.Open();
+                    SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    con.Close();
+                    dataGridView1.DataSource = dt;
+                }
+            }
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            using (SqlConnection con = new SqlConnection(cs))
+            using (con = new SqlConnection(cs))
             {
-                con.Open()
+                string cmdText = "insert into Addmission(FirstName,LastName,Address,Country,PinCode) values (@FirstName,@LastName,@Address,@Country,@PinCode);";
+                using (cmd = new SqlCommand(cmdText, con))
+                {
+                    cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text.Trim());
+                    cmd.Parameters.AddWithValue("@LastName", txtLastName.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Country", txtCountry.Text.Trim());
+                    cmd.Parameters.AddWithValue("@PinCode", Convert.ToInt32(txtPinCode.Text));
 
+                    con.Open();
+                    int response = cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    if (response > 0)
+                    {
+                        MessageBox.Show("Data Inserted.");
+                        DataShow();
+                    }
+                }
             }
         }
     }
